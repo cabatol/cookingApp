8c4e119c6606f68082f6a6b5cf2db9807ae9bcbb
Your <cabatol@csumb.edu>
2017-03-29 21:40:16 +0000

    Added basic first page we can work with

---
+++ ProjectMainFile.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+btn1 = Button(text="Add Recipe")
+btn2 = Button(text="Search Recipes")
+btn3 = Button(text="Learn to Cook")
+btn4 = Button(text="More...")
+
+btn1.place(relx=0.5, rely=0.2, anchor=CENTER)
+btn2.place(relx=0.5, rely=0.4, anchor=CENTER)
+btn3.place(relx=0.5, rely=0.6, anchor=CENTER)
+btn4.place(relx=0.5, rely=0.8, anchor=CENTER)
+                    
+mainloop()
