ec64ee35e73df140b6770353b223fc4e214399de
Your <cabatol@csumb.edu>
2017-03-30 00:46:17 +0000

    added complexity to home page using kivy

--- Firstpage.py
+++ Firstpage.py
@@ -1,17 +1,40 @@
-import sys
-from tkinter import *
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-def addRecipe():
-    mlabel1= Label(mGui, text='you cant do this yet').pack()
+class myLayout(BoxLayout):
+    def __init__ (self, **kwargs):
+        super(myLayout, self).__init__(**kwargs)
 
-mGui = Tk()
+        btn1 = Button(text = "Search for a Recipe")
+        btn1.bind(on_press=self.clk1)
+        btn2 = Button(text = "Add a Recipe")
+        btn2.bind(on_press=self.clk2)
+        btn3 = Button(text = "Learn to Cook")
+        btn3.bind(on_press=self.clk3)
+        btn4 = Button(text = "More...")
+        btn4.bind(on_press=self.clk4)
 
-mGui.geometry('325x500')
-mGui.title('My Cooking App')
+        self.add_widget(Label(text="Welcome to My Cooking App"))
+        self.add_widget(btn1)
+        self.add_widget(btn2)
+        self.add_widget(btn3)
+        self.add_widget(btn4)
 
-mlabel = Label(mGui, text='My Cooking App').place(relx=0.5, rely=.1, anchor=CENTER)
-btn1 = Button(mGui, text='Add Recipe', command = addRecipe).place(relx=0.5, rely=0.2, anchor=CENTER)
-btn2 = Button(mGui, text='Search Recipe', command = addRecipe).place(relx=0.5, rely=0.3, anchor=CENTER)
-btn3 = Button(mGui, text='Learn to Cook', command = addRecipe).place(relx=0.5, rely=0.4, anchor=CENTER)
-btn4 = Button(mGui, text='More...', command = addRecipe).place(relx=0.5, rely=0.5, anchor=CENTER)
+    def clk1(self, obj):
+        print("Hi There")
+    def clk2(self, obj):
+        print("Hi There!")
+    def clk3(self, obj):
+        print("Hi There!!")
+    def clk4(self, obj):
+        print("Hi There!!!")
 
+class NameApp(App):
+    def build(self):
+        mL = myLayout()
+        return mL
+
+if __name__ == "__main__":
+    NameApp().run()
