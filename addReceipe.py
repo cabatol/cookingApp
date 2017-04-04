72af56def20c328d0551ab21c3494157eb597472
Judith Ramirez <judramirez@csumb.edu>
2017-04-04 00:44:37 +0000

    Add recipe page

--- addReceipe.py
+++ addReceipe.py
@@ -1,24 +1,53 @@
+from kivy.app import App
+from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 
 
-class CookingApp(App):
+def on_text(instance,value):
+    print(instance,value)
+    
+class addRecipe(App):
     def build(self):
         f = FloatLayout()
-        saveB = Button(text = "Save", size_hint =(.1,.1), pos = (290,25))
-        cancelB = Button(text = "Cancel", size_hint =(.1,.1), pos =(400,25))
-        name = TextInput(text = "Name of recipe",font_size = 10, multiline = False, size_hint = (.5,.05),pos =(200,455))
-        servings = TextInput(text= "Servings",font_size = 10, multiline = False, size_hint = (.1,.05),pos =(299,400))
-        time = TextInput(text = "Time required",font_size = 10, multiline = False, size_hint = (.1,.05), pos =(435, 400))
-        ingredients = TextInput(text = "Ingredients",font_size = 10, size_hint = (.5,.05), pos =(200, 355))
-        directions = TextInput(text = "Directions",font_size = 10, size_hint = (.5,.3), pos =(200,155))
-        #saveB.bind(on_press = self.clk)
-        #cancelB.bind(on_press = self.click1)
+        saveB = Button(text = "Save",
+                       size_hint =(.1,.1),
+                       pos_hint = {'x':.35,'y':.05})
+        saveB.bind(on_press = self.clk)
+        
+        cancelB = Button(text = "Cancel",
+                         size_hint =(.1,.1),
+                         pos_hint = {'x':.55, 'y':.05})
+        
+        name = TextInput(size_hint = (.7,.05),
+                         pos_hint = {'x':.15, 'y':.8})
+        name.bind(text=on_text)
+        
+        servings = TextInput(text= "Servings",
+                             font_size = 10,
+                             multiline = False,
+                             size_hint = (.1,.05),
+                             pos_hint = {'x':.3, 'y':.7})
+        time = TextInput(text = "Time required",
+                         font_size = 10,
+                         multiline = False,
+                         size_hint = (.1,.05),
+                         pos_hint = {'x':.15, 'y':.7})
+        ingredients = TextInput(text = "Ingredients",
+                                font_size = 10,
+                                size_hint = (.7,.05),
+                                pos_hint = {'x':.15, 'y':.6})
+        directions = TextInput(text = "Directions",
+                               font_size = 10,
+                               size_hint = (.7,.35),
+                               pos_hint = {'x':.15, 'y':.2})
+        #cancelB.bind(on_press = self.clk)
 
         
-        f.add_widget(Label(text = "Add a Recipe", font_size = 45, size_hint=(1,1.9)))
+        f.add_widget(Label(text = "Add a Recipe", font_size = 45,size_hint=(1,1.9)))
         f.add_widget(saveB)
         f.add_widget(cancelB)
         f.add_widget(name)
@@ -26,13 +55,17 @@ class CookingApp(App):
         f.add_widget(time)
         f.add_widget(ingredients)
         f.add_widget(directions)
-        
 
         return f
-    #def clk(self,obj):
-        #print("Hello Wo")
+    
 
+    def clk(self,obj):
+        popup = Popup(title = "Confirmation",
+                      content=Label(text = "Saved!"),
+                      size_hint = (None,None),
+                      size=(200,150))
+        popup.open()
 
 
 if __name__ == "__main__":
-    CookingApp().run()
+    addRecipe().run()
