8ec50c75f21be59b7dc4402a96b96623d7aa2be6
Judith Ramirez <judramirez@csumb.edu>
2017-04-20 02:25:55 +0000

    Yummly API- User input

--- ApiTest.py
+++ ApiTest.py
@@ -4,13 +4,32 @@ import json
 my_key = '8b6f178c57d40dee7d88629b32e01c23'
 my_id = 'fabbe897'
 
-#my_search= {
-#     "id":"course-Appetizers",
-#     "description":"Appetizers",
-#    "searchValue":"course^course-Appetizers"
+#raw_answer = raw_input()#self.words.txt.split(' ')
+recipe = 'orange chicken'
+#raw_answer.split(' ')
+my_search= {
+    'q' : recipe,
+    'requirePictures': 'True',
+    'allowedIngredient[]': recipe
+    
+}
+
+#search = {
+#'q' : recipe,
+#'requirePictures': ,
+#'allowedIngredient[]': ,
+#'excludedIngredient[]' : ,
+#'allowedAllergy[]' : ,
+#'allowedDiet[]' : ,
+#'allowedCuisine[]' : ,
+#'excludedCuisine[]' : ,
+#'allowedCourse[]' : ,
+#'excludedCourse[]' : ,
+#'maxTotalTimeInSeconds' : ,
 #}
 
-r = requests.get('http://api.yummly.com/v1/api/recipes?_app_id=fabbe897&_app_key=8b6f178c57d40dee7d88629b32e01c23&q=onion%20soup&requirePictures=true&allowedIngredient[]=garlic')
+r = requests.get('http://api.yummly.com/v1/api/recipes?_app_id=fabbe897&_app_key=8b6f178c57d40dee7d88629b32e01c23&',params = my_search)
+
 
 info = r.json()
 print(info)
